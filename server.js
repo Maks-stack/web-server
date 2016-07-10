264b6e77b3a8c3b1501781034aa03179c328bdb0
Max <elmaxo@hotmail.de>
2016-07-10 17:19:10 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next)
+	{
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req,res,next)
+	{
+		var date = new Date().toString();
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + "on: " + date);
+		next();
+	}
+}
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res)
+	{
+		res.send('about us');
+	});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function()
+	{
+		console.log("Express Server Started on port: " + PORT);
+	});
