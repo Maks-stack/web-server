3354c25a5f38b48e3c9112c761c4f1d92ed030ef
Max <elmaxo@hotmail.de>
2016-07-10 17:45:56 +0200

    refactor middleware

--- server.js
+++ server.js
@@ -2,19 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-	requireAuthentication: function(req, res, next)
-	{
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req,res,next)
-	{
-		var date = new Date().toString();
-		console.log('Request: ' + req.method + ' ' + req.originalUrl + "on: " + date);
-		next();
-	}
-}
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
@@ -30,3 +18,4 @@ app.listen(PORT, function()
 	{
 		console.log("Express Server Started on port: " + PORT);
 	});
+
